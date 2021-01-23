1c9353d58bfd7a9c5fcb6a18f7921b7eb63762ee
AlexchooN14 <33004020+AlexchooN14@users.noreply.github.com>
2021-01-23 11:14:59 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqllite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(30), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r' % self.username
+
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        User(username=username, password=password)
+    return render_template("register.html")
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
