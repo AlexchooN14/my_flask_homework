46d053f46381969a9dc24c6e353ab24db0ab4d8a
AlexchooN14 <33004020+AlexchooN14@users.noreply.github.com>
2021-01-27 00:10:40 +0200

    WORKING, now let's design

--- app.py
+++ app.py
@@ -63,6 +63,53 @@ def register():
         return redirect('/')
 
 
+@app.route('/posts')
+def list_posts():
+    return render_template('posts.html', posts=Post.all())
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    post = Post.find(post_id)
+
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.find(post_id)
+    if request.method == 'GET':
+        return render_template('edit_post.html', post=post)
+    elif request.method == 'POST':
+        post.name = request.form['name']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        post.save()
+
+        return redirect(url_for('show_post', post_id=post.post_id))
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    post = Post.find(post_id)
+    post.delete()
+
+    return redirect(url_for('list_posts'))
+
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+@auth.login_required
+def new_post():
+    if request.method == 'GET':
+        return render_template('new_post.html')
+
+    elif request.method == 'POST':
+        logging.info("stignahme do post")
+        values = (None, request.form['name'], request.form['author'], request.form['content'])
+        Post(*values).create()
+        return redirect(url_for('list_posts'))
+
+
 @auth.verify_password
 def verify_password(username, password):
     user = User.find_by_username(username)
